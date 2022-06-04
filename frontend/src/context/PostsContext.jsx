a48b19cd9655f6b907ec9931bf7c8dbfea2b36e9
binamralamsal <binamralamsal@gmail.com>
2022-06-04 21:17:00 +0545

    Fixed issue related to like button not working

--- frontend/src/context/PostsContext.jsx
+++ frontend/src/context/PostsContext.jsx
@@ -1,61 +1,62 @@
 import { createContext, useState, useContext } from "react";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext"
+import { AuthContext } from "../context/AuthContext";
 
 export const PostsContext = createContext();
 
 const PostsProvider = ({ children }) => {
-    const [posts, setPosts] = useState([])
-    const { user } = useContext(AuthContext)
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
-    const getPosts = async () => {
-        try {
-            const { data } = await axios.get("/api/posts")
-            setPosts(data);
-        } catch (error) {
-            // toast.error(error.response.data.message);
-        }
+  const getPosts = async () => {
+    try {
+      const { data } = await axios.get("/api/posts");
+      setPosts(data);
+    } catch (error) {
+      // toast.error(error.response.data.message);
     }
+  };
 
-    const createPost = async (content, image, githubLink, liveDemoLink) => {
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${user.token}`,
-                }
-            }
-            const result = await axios.post("/api/posts/create", { content, image, githubLink, liveDemoLink }, config);
-            console.log(result)
-        } catch (error) {
-            // Will do later
-        }
+  const createPost = async (content, image, githubLink, liveDemoLink) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const result = await axios.post(
+        "/api/posts/create",
+        { content, image, githubLink, liveDemoLink },
+        config
+      );
+    } catch (error) {
+      // Will do later
     }
+  };
 
-    const likePost = async (_id) => {
-        try {
-            console.log(_id);
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                }
-            }
-            const result = await axios.put(`/api/posts/like/${_id}`, null, config);
-            console.log(result.data)
-            return result.data;
-        } catch (error) {
-            // Will do later
-            console.log(error)
-        }
+  const likePost = async (_id) => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const result = await axios.put(`/api/posts/like/${_id}`, null, config);
+      return result.data;
+    } catch (error) {
+      // Will do later
+      console.log(error);
     }
+  };
 
-    return (
-        <PostsContext.Provider
-            value={{ posts, createPost, getPosts, likePost, setPosts }}
-        >
-            {children}
-        </PostsContext.Provider>
-    );
+  return (
+    <PostsContext.Provider
+      value={{ posts, createPost, getPosts, likePost, setPosts }}
+    >
+      {children}
+    </PostsContext.Provider>
+  );
 };
 
 export default PostsProvider;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,88 +1,104 @@
 import { useContext, useEffect } from "react";
 import { PostsContext } from "../context/PostsContext";
 import { AuthContext } from "../context/AuthContext";
-import styles from '../styles/pages/Home.module.css'
-import TimeAgo from 'react-timeago'
+import styles from "../styles/pages/Home.module.css";
+import TimeAgo from "react-timeago";
 
-import { AiOutlineHeart, AiFillHeart, AiFillGithub, AiOutlineEye } from 'react-icons/ai'
-import { GoCommentDiscussion } from 'react-icons/go'
+import {
+  AiOutlineHeart,
+  AiFillHeart,
+  AiFillGithub,
+  AiOutlineEye,
+} from "react-icons/ai";
+import { GoCommentDiscussion } from "react-icons/go";
 
 const Home = () => {
-    const { posts, getPosts, likePost, setPosts } = useContext(PostsContext)
-    const { user } = useContext(AuthContext)
+  const { posts, getPosts, likePost } = useContext(PostsContext);
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        getPosts()
-    }, [])
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    const handleLikeBtnClick = async (postId, index) => {
-        const data = await likePost(postId);
-        console.log(postId, index)
-        // const updatedPosts = [...posts];
-        // console.log(data)
+  const handleLikeBtnClick = async (postId, index) => {
+    const data = await likePost(postId);
 
-        // if (data.userLiked) {
-        //     updatedPosts[index].likes.push(user._id)
-        // } else {
-        //     updatedPosts[index].likes = updatedPosts[index].likes.filter(id => id !== user._id)
-        // }
+    getPosts();
+  };
 
-        // console.log("updated", updatedPosts);
-        // setPosts(updatedPosts)
-
-        getPosts()
-    }
-
-    console.log(posts)
-
-    return (
-        <div className={styles.homepage}>
-            <div className={styles.posts}>
-                {posts.map((post, index) => (
-                    <div key={post._id} className={styles.post}>
-                        <div className={styles.postUpper}>
-                            <p className={styles.postAuthor}>{post.userId.name}</p>
-                            <p className={styles.time}><TimeAgo date={post.createdAt} /></p>
-                        </div>
-                        <div className={styles.postContent}>
-                            <p className={styles.postText}>{post.content}</p>
-                            {post.image && (
-                                <a href={post.image} target="_blank">
-                                    <img src={post.image} alt='Post image' className={styles.postImage} />
-                                </a>
-                            )}
-                        </div>
-                        <div className={styles.postBottom}>
-                            <span>
-                                {post.likes.includes(user._id) ? <AiFillHeart className={styles.icons} size={21} color='#4385FF' onClick={() => handleLikeBtnClick(post._id, index)} /> : <AiOutlineHeart className={styles.icons} size={21} color='#4385FF' onClick={() => handleLikeBtnClick(post._id, index)} />}
-                                {post.likes.length}
-                            </span>
-                            <GoCommentDiscussion className={styles.icons} size={21} color='#4385FF' />
-                            {post.githubLink && (
-                                <a href={post.githubLink} target='_blank'>
-                                    <AiFillGithub className={styles.icons} size={21} />
-                                </a>
-                            )}
-                            {post.liveDemoLink && (
-                                <a href={post.liveDemoLink} target='_blank'>
-                                    <AiOutlineEye className={styles.icons} size={21} />
-                                </a>
-                            )}
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.posts}>
+        {posts.map((post, index) => (
+          <div key={post._id} className={styles.post}>
+            <div className={styles.postUpper}>
+              <p className={styles.postAuthor}>{post.userId.name}</p>
+              <p className={styles.time}>
+                <TimeAgo date={post.createdAt} />
+              </p>
             </div>
-            <div className={styles.leaderboard}>
-                <p className={styles.title}>Leaderboard ✨</p>
-                <p className={styles.users}>1. Binamra</p>
-                <p className={styles.users}>2. Aman</p>
-                <p className={styles.users}>3. Lorem</p>
-                <p className={styles.users}>4. Ipsum</p>
-                <p className={styles.users}>5. Dolor</p>
-                <p className={styles.users}>6. Sit</p>
+            <div className={styles.postContent}>
+              <p className={styles.postText}>{post.content}</p>
+              {post.image && (
+                <a href={post.image} target="_blank">
+                  <img
+                    src={post.image}
+                    alt="Post image"
+                    className={styles.postImage}
+                  />
+                </a>
+              )}
             </div>
-        </div >
-    );
+            <div className={styles.postBottom}>
+              <button onClick={() => handleLikeBtnClick(post._id, index)}>
+                {post.likes.includes(user._id) ? (
+                  <AiFillHeart
+                    className={styles.icons}
+                    size={21}
+                    color="#4385FF"
+                  />
+                ) : (
+                  <AiOutlineHeart
+                    className={styles.icons}
+                    size={21}
+                    color="#4385FF"
+                    // onClick={() => handleLikeBtnClick(post._id, index)}
+                  />
+                )}
+                {post.likes.length}
+              </button>
+              <button>
+                <GoCommentDiscussion
+                  className={styles.icons}
+                  size={21}
+                  color="#4385FF"
+                />
+              </button>
+              {post.githubLink && (
+                <a href={post.githubLink} target="_blank">
+                  <AiFillGithub className={styles.icons} size={21} />
+                </a>
+              )}
+              {post.liveDemoLink && (
+                <a href={post.liveDemoLink} target="_blank">
+                  <AiOutlineEye className={styles.icons} size={21} />
+                </a>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.leaderboard}>
+        <p className={styles.title}>Leaderboard ✨</p>
+        <p className={styles.users}>1. Binamra</p>
+        <p className={styles.users}>2. Aman</p>
+        <p className={styles.users}>3. Lorem</p>
+        <p className={styles.users}>4. Ipsum</p>
+        <p className={styles.users}>5. Dolor</p>
+        <p className={styles.users}>6. Sit</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
